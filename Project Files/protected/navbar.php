bd0887c334a1400512867c5e8bd6a34e77fbddb3
Oleg <oleg.dragoi228@gmail.com>
2021-10-04 22:34:24 +0200

    Developed navigation bar
    Added navbar.php

---
+++ Project Files/protected/navbar.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a class="d-flex align-items-center mb-3 mb-md-0 text-dark text-decoration-none" href="/">
+        <svg class="bi me-2" width="40" height="32">
+          <use xlink:href="#bootstrap" />
+        </svg>
+        <span class="fs-4">Pied Piper</span>
+  
+      </a>
+      <div class="me-md-auto"><a href="?P=articles" class="nav-link">Articles</a></div>
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="?P=home#" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="?P=home#Features" class="nav-link">Features</a></li>
+        <li class="nav-item"><a href="?P=home#Pricing" class="nav-link">Pricing</a></li>
+        <li class="nav-item"><a href="?P=home#Sign up" class="nav-link">Sign up</a></li>
+        <li class="nav-item"><a href="?P=about_us" class="nav-link">About us</a></li>
+      </ul>
+    </header>
+</div>
