7357cf7942f1bea6360b6210214ab3ccaf02be09
AhmedMahfouz <ahmelsamahy@gmail.com>
2021-10-05 21:32:28 +0200

    modified to footer
    added articles to it and navigated to php files

--- Project Files/protected/footer.php
+++ Project Files/protected/footer.php
@@ -1,10 +1,11 @@
 <div class="container">
     <footer class="py-3 my-4">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <li class="nav-item"><a href="#Top" class="nav-link px-2 text-muted">Home</a></li>
-        <li class="nav-item"><a href="#Features" class="nav-link px-2 text-muted">Features</a></li>
-        <li class="nav-item"><a href="#Pricing" class="nav-link px-2 text-muted">Pricing</a></li>
-        <li class="nav-item"><a href="#Sign up" class="nav-link px-2 text-muted">Sign Up</a></li>
+        <li class="nav-item"><a href="?P=home#" class="nav-link px-2 text-muted">Home</a></li>
+        <li class="nav-item"><a href="?P=articles" class="nav-link px-2 text-muted">Articles</a></li>
+        <li class="nav-item"><a href="?P=home#Features" class="nav-link px-2 text-muted">Features</a></li>
+        <li class="nav-item"><a href="?P=home#Pricing" class="nav-link px-2 text-muted">Pricing</a></li>
+        <li class="nav-item"><a href="?P=home#Sign up" class="nav-link px-2 text-muted">Sign Up</a></li>
         <li class="nav-item"><a href="./About us.html" class="nav-link px-2 text-muted">About us</a></li>
     </ul>
     <p class="text-center text-muted">&copy; 2021 Pied Piper, Inc</p>
