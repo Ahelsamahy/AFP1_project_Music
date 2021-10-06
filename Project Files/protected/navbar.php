20acf2582bdf8fb9ead6754fd8205de0bfbfe054
AhmedMahfouz <ahmelsamahy@gmail.com>
2021-10-06 23:22:22 +0200

    removed sign up
    and replaced it with sign in first then the user can choose sign up

--- Project Files/protected/navbar.php
+++ Project Files/protected/navbar.php
@@ -13,8 +13,7 @@
         <li class="nav-item"><a href="?P=articles" class="nav-link">Articles</a></li>
         <li class="nav-item"><a href="?P=home#Features" class="nav-link">Features</a></li>
         <li class="nav-item"><a href="?P=home#Pricing" class="nav-link">Pricing</a></li>
-        <li class="nav-item"><a href="?P=login" class="nav-link">Log in</a></li>
-        <li class="nav-item"><a href="?P=home#Sign up" class="nav-link">Sign up</a></li>
+        <li class="nav-item"><a href="?P=Sign_in" class="nav-link">Sign in</a></li>
         <li class="nav-item"><a href="?P=about_us" class="nav-link">About us</a></li>
 
       </ul>
