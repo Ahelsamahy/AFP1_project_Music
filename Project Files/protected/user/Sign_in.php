d8b441d7caa6889aca0f5519e7c09e37b8c66545
AhmedMahfouz <ahmelsamahy@gmail.com>
2021-10-06 23:23:54 +0200

    renamed the file
    from login to sign in and changed in the layout, some css touches

--- Project Files/protected/user/Sign_in.php
+++ Project Files/protected/user/Sign_in.php
@@ -17,38 +17,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login']))
 ?>
 <div class="container">
 <h1 class="h1edit"> Login </h1>
-<hr>
-<form method="post">
-  <div class="form-group">
-    <label for="loginUsername">Username</label>
-    <input type="text" class="form-control" id="loginUsername" aria-describedby="usernameHelp"
-     name="username" value="<?= isset($postData) ? $postData['username'] : '';?>">
-  </div>
-  <div class="form-group">
-    <label for="loginPassword">Password</label>
-    <input type="password" class="form-control" id="loginPassword" name="password" value="">
-  </div>
-  <div>
-  <button id="genny" type="submit" class="btn btn-primary" name="login">Login</button>
-  </div>
-</form>
+    <center>
+        <hr class="mb-5 mt-3" style="width:15%">
+      </center>
 
-<!-- ************************************************************************************** -->
-<div class="col-md-10 mx-auto col-lg-5">
+<div class="col-d-10 mx-auto col-lg-4 ">
   <form class="p-4 p-md-5 border rounded-3 bg-light">
     <div class="form-floating mb-3">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <input type="email" class="form-control" id="loginUsername" aria-describedby="usernameHelp"
+     name="username" value="<?= isset($postData) ? $postData['username'] : '';?>">
       <label for="loginUsername">Email address</label>
     </div>
     <div class="form-floating mb-3">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <input type="password" class="form-control" id="loginPassword" name="password" value="">
       <label for="floatingPassword">Password</label>
     </div>
-    <div class="checkbox mb-3">
+    <!-- can be implemented later -->
+    <!-- <div class="checkbox mb-3">
       <label>
         <input type="checkbox" value="remember-me"> Remember me
       </label>
-    </div>
+    </div> -->
     <button class="w-100 btn btn-lg btn-primary" type="submit">Sign In</button>
     <hr class="my-4">
    <center> <small class="text-muted">if you don't have account, you can 
