522f6c11fed5fe59fed4f4316bd80bc0866f202d
AhmedMahfouz <ahmelsamahy@gmail.com>
2021-10-07 00:35:14 +0200

    started with sign up page
    will need form with info of the user

---
+++ Project Files/protected/normal/Sign_Up.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Sign Up</title>
+    <link rel="shortcut icon" href="./Used images/sml.jpg">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap core CSS -->
+    <link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+  <!-- Custom styles for this template -->
+  <link href="<?=PUBLIC_DIR.'index.css'?>" rel="stylesheet">
+
+   <!--- Bootrstrap javascript --->
+  <script src="./assets/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <?php require_once PROTECTED_DIR.'navbar.php'; ?>
+    <div class="px-4 py-5 my-5 text-center" id="Top">
+      <!-- will start with it, just testing -->
+
+    </div>
+    <footer><?php include_once PROTECTED_DIR.'footer.php'; ?></footer>
+    
+</body>
+</html>
