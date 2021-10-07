57e2681b79ef83f4e926b7545dcf26f5e7f046af
AhmedMahfouz <ahmelsamahy@gmail.com>
2021-10-07 11:27:14 +0200

    removed buttons
    as they are not used and can be replaced with input.
    also made them floating input to add smoothness

--- Project Files/protected/normal/Sign_Up.php
+++ Project Files/protected/normal/Sign_Up.php
@@ -17,16 +17,8 @@
                 <div class="row g-0">
                   <div class="col-lg-6">
                     <div class="p-5">
-                      <h3 class="fw-normal mb-5" style="color: #4835d4;">General Infomation</h3>
+                      <h3 class="fw-normal mb-5">General Infomation</h3>
 
-                      <div class="mb-4 pb-2">
-                        <select class="select">
-                          <option value="1">Title</option>
-                          <option value="2">Two</option>
-                          <option value="3">Three</option>
-                          <option value="4">Four</option>
-                        </select>
-                      </div>
 
                       <div class="row">
                         <div class="col-md-6 mb-4 pb-2">
@@ -46,18 +38,10 @@
                         </div>
                       </div>
 
-                      <div class="mb-4 pb-2">
-                        <select class="select">
-                          <option value="1">Position</option>
-                          <option value="2">Two</option>
-                          <option value="3">Three</option>
-                          <option value="4">Four</option>
-                        </select>
-                      </div>
 
                       <div class="mb-4 pb-2">
-                        <div class="form-outline">
-                          <input type="text" id="form3Examplev4" class="form-control form-control-lg" />
+                        <div class="form-floating">
+                          <input type="text" id="form3Examplev4" class="form-control form-control-lg" placeholder="Position" />
                           <label class="form-label" for="form3Examplev4">Position</label>
                         </div>
                       </div>
@@ -65,40 +49,32 @@
                       <div class="row">
                         <div class="col-md-6 mb-4 pb-2 mb-md-0 pb-md-0">
 
-                          <div class="form-outline">
-                            <input type="text" id="form3Examplev5" class="form-control form-control-lg" />
-                            <label class="form-label" for="form3Examplev5">Bussines Arena</label>
+                          <div class="form-floating">
+                            <input type="text" id="form3Examplev5" class="form-control form-control-lg" placeholder="Business Arena" />
+                            <label class="form-label" for="form3Examplev5">Business Arena</label>
                           </div>
 
                         </div>
-                        <div class="col-md-6">
 
-                          <select class="select">
-                            <option value="1">Employees</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                            <option value="4">Four</option>
-                          </select>
-
-                        </div>
                       </div>
 
                     </div>
                   </div>
-                  <div class="col-lg-6 bg-indigo text-white">
+
+                  <div class="col-lg-6 bg-white " style="background-color: #0d6efd;">
                     <div class="p-5">
                       <h3 class="fw-normal mb-5">Contact Details</h3>
 
                       <div class="mb-4 pb-2">
-                        <div class="form-outline form-white">
-                          <input type="text" id="form3Examplea2" class="form-control form-control-lg" />
+                        <div class="form-floating form-white">
+                          <input type="text" id="form3Examplea2" class="form-control form-control-lg" placeholder="Street + Nr" />
                           <label class="form-label" for="form3Examplea2">Street + Nr</label>
                         </div>
                       </div>
 
                       <div class="mb-4 pb-2">
-                        <div class="form-outline form-white">
-                          <input type="text" id="form3Examplea3" class="form-control form-control-lg" />
+                        <div class="form-floating form-white">
+                          <input type="text" id="form3Examplea3" class="form-control form-control-lg" placeholder="Additional Information" />
                           <label class="form-label" for="form3Examplea3">Additional Information</label>
                         </div>
                       </div>
@@ -106,16 +82,16 @@
                       <div class="row">
                         <div class="col-md-5 mb-4 pb-2">
 
-                          <div class="form-outline form-white">
-                            <input type="text" id="form3Examplea4" class="form-control form-control-lg" />
+                          <div class="form-floating form-white">
+                            <input type="text" id="form3Examplea4" class="form-control form-control-lg" placeholder="Zip Code" />
                             <label class="form-label" for="form3Examplea4">Zip Code</label>
                           </div>
 
                         </div>
                         <div class="col-md-7 mb-4 pb-2">
 
-                          <div class="form-outline form-white">
-                            <input type="text" id="form3Examplea5" class="form-control form-control-lg" />
+                          <div class="form-floating form-white">
+                            <input type="text" id="form3Examplea5" class="form-control form-control-lg" placeholder="Place" />
                             <label class="form-label" for="form3Examplea5">Place</label>
                           </div>
 
@@ -123,8 +99,8 @@
                       </div>
 
                       <div class="mb-4 pb-2">
-                        <div class="form-outline form-white">
-                          <input type="text" id="form3Examplea6" class="form-control form-control-lg" />
+                        <div class="form-floating form-white">
+                          <input type="text" id="form3Examplea6" class="form-control form-control-lg" placeholder="Country" />
                           <label class="form-label" for="form3Examplea6">Country</label>
                         </div>
                       </div>
@@ -132,16 +108,16 @@
                       <div class="row">
                         <div class="col-md-5 mb-4 pb-2">
 
-                          <div class="form-outline form-white">
-                            <input type="text" id="form3Examplea7" class="form-control form-control-lg" />
+                          <div class="form-floating form-white">
+                            <input type="text" id="form3Examplea7" class="form-control form-control-lg" placeholder="Code +" />
                             <label class="form-label" for="form3Examplea7">Code +</label>
                           </div>
 
                         </div>
                         <div class="col-md-7 mb-4 pb-2">
 
-                          <div class="form-outline form-white">
-                            <input type="text" id="form3Examplea8" class="form-control form-control-lg" />
+                          <div class="form-floating form-white">
+                            <input type="text" id="form3Examplea8" class="form-control form-control-lg" placeholder="Phone Number" />
                             <label class="form-label" for="form3Examplea8">Phone Number</label>
                           </div>
 
@@ -149,16 +125,16 @@
                       </div>
 
                       <div class="mb-4">
-                        <div class="form-outline form-white">
-                          <input type="text" id="form3Examplea9" class="form-control form-control-lg" />
+                        <div class="form-floating form-white">
+                          <input type="text" id="form3Examplea9" class="form-control form-control-lg" placeholder="Your Email" />
                           <label class="form-label" for="form3Examplea9">Your Email</label>
                         </div>
                       </div>
 
                       <div class="form-check d-flex justify-content-start mb-4 pb-3">
                         <input class="form-check-input me-3" type="checkbox" value="" id="form2Example3c" />
-                        <label class="form-check-label text-white" for="form2Example3">
-                          I do accept the <a href="#" class="text-white"><u>Terms and Conditions</u></a> of your site.
+                        <label class="form-check-label text-black" for="form2Example3">
+                          I do accept the <a href="#" class="text-black"><u>Terms and Conditions</u></a> of your site.
                         </label>
                       </div>
 
