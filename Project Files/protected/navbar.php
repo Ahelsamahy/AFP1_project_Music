10a3d96ccc71d68d9dbe1a9d5688e122e296583c
AhmedMahfouz <ahmelsamahy@gmail.com>
2021-10-07 16:39:56 +0200

    changed order
    of the articles so it will be after all the smooth transaction in the page then it will take you to another page in the end

--- Project Files/protected/navbar.php
+++ Project Files/protected/navbar.php
@@ -1,21 +1,21 @@
 <div class="container">
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none" href="#">
-        <svg class="bi me-2" width="40" height="32">
-          <use xlink:href="#bootstrap" />
-        </svg>
-        <span class="fs-4">Pied Piper</span>
-  
-      </a>
-      <ul class="nav nav-pills">
-      <!-- aria-current="page" -->
-        <li class="nav-item"><a href="?P=home#" class="nav-link">Home</a></li>
-        <li class="nav-item"><a href="?P=articles" class="nav-link">Articles</a></li>
-        <li class="nav-item"><a href="?P=home#Features" class="nav-link">Features</a></li>
-        <li class="nav-item"><a href="?P=home#Pricing" class="nav-link">Pricing</a></li>
-        <li class="nav-item"><a href="?P=Sign_in" class="nav-link">Sign in</a></li>
-        <li class="nav-item"><a href="?P=about_us" class="nav-link">About us</a></li>
+  <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <a class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none" href="#">
+      <svg class="bi me-2" width="40" height="32">
+        <use xlink:href="#bootstrap" />
+      </svg>
+      <span class="fs-4">Pied Piper</span>
 
-      </ul>
-    </header>
+    </a>
+    <ul class="nav nav-pills">
+      <!-- aria-current="page" -->
+      <li class="nav-item"><a href="?P=home#" class="nav-link">Home</a></li>
+      <li class="nav-item"><a href="?P=home#Features" class="nav-link">Features</a></li>
+      <li class="nav-item"><a href="?P=home#Pricing" class="nav-link">Pricing</a></li>
+      <li class="nav-item"><a href="?P=articles" class="nav-link">Articles</a></li>
+      <li class="nav-item"><a href="?P=Sign_in" class="nav-link">Sign in</a></li>
+      <li class="nav-item"><a href="?P=about_us" class="nav-link">About us</a></li>
+
+    </ul>
+  </header>
 </div>
