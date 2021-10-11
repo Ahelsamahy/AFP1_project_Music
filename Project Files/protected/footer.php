727dbe2c41e967dab365546cb468ff73b7e3c7c6
Oleg <oleg.dragoi228@gmail.com>
2021-10-11 23:07:29 +0200

    Edited footer
    user is logged in => "Sign in" not shown in the footer

--- Project Files/protected/footer.php
+++ Project Files/protected/footer.php
@@ -5,7 +5,9 @@
             <li class="nav-item"><a href="?P=home#Features" class="nav-link px-2 text-muted">Features</a></li>
             <li class="nav-item"><a href="?P=home#Pricing" class="nav-link px-2 text-muted">Pricing</a></li>
             <li class="nav-item"><a href="?P=articles" class="nav-link px-2 text-muted">Articles</a></li>
-            <li class="nav-item"><a href="?P=Sign_in" class="nav-link px-2 text-muted">Sign in</a></li>
+            <?php if(!IsUserLoggedIn()) :?>
+		<li class="nav-item"><a href="?P=Sign_in" class="nav-link px-2 text-muted">Sign in</a></li>
+	    <?php endif; ?>
             <li class="nav-item"><a href="?P=about_us" class="nav-link px-2 text-muted">About us</a></li>
         </ul>
         <p class="text-center text-muted">&copy; 2021 Pied Piper, Inc</p>
