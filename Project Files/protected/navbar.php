c7c21aeda56fb083108ff46ca3c48d7653d1075c
Oleg <oleg.dragoi228@gmail.com>
2021-10-11 22:15:06 +0200

    Edited navbar
    it now shows (Sign in)/(Add article; Logout) when a user is logged in / not

--- Project Files/protected/navbar.php
+++ Project Files/protected/navbar.php
@@ -13,9 +13,14 @@
       <li class="nav-item"><a href="?P=home#Features" class="nav-link">Features</a></li>
       <li class="nav-item"><a href="?P=home#Pricing" class="nav-link">Pricing</a></li>
       <li class="nav-item"><a href="?P=articles" class="nav-link">Articles</a></li>
-      <li class="nav-item"><a href="?P=Sign_in" class="nav-link">Sign in</a></li>
+	<?php if(!IsUserLoggedIn()) :?>
+		<li class="nav-item"><a href="?P=Sign_in" class="nav-link">Sign in</a></li>
+	<?php endif; ?>
+	<?php if(IsUserLoggedIn()) :?>	
+		<li class="nav-item"><a href="?P=add_article" class="nav-link">Add Article</a></li>	
+		<li class="nav-item"><a href="index.php?P=logout" class="nav-link">Logout</a>
+	<?php endif;?>
       <li class="nav-item"><a href="?P=about_us" class="nav-link">About us</a></li>
-
     </ul>
   </header>
 </div>
